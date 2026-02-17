e738722ed4f0f9a51fe59bd830e2eeed1dcf95ae
rashmin <rashminxperience@gmail.com>
2026-02-17 20:45:34 +0545

    Added Chat UI and Username while creating account

---
+++ lib/widgets/new_messages.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class NewMessages extends StatefulWidget {
+  const NewMessages({super.key});
+
+  @override
+  State<NewMessages> createState() => _NewMessagesState();
+}
+
+class _NewMessagesState extends State<NewMessages> {
+  var _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() {
+    final enteredMessage = _messageController.text;
+    if(enteredMessage.trim().isEmpty) {
+      return;
+    }
+    // send to Firebase
+    _messageController.clear();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: EdgeInsets.only(left: 15,right: 1,bottom: 14),
+      child: Row(children: [
+      Expanded(child: TextField(
+        controller: _messageController,
+        textCapitalization: TextCapitalization.sentences,
+        autocorrect: true,
+        enableSuggestions: true,
+        decoration: InputDecoration(labelText: 'Send a message...'),
+      )),
+        IconButton(
+            color: Theme.of(context).colorScheme.primary,
+            onPressed: () {}, icon: Icon(Icons.send))
+    ],),);
+  }
+}
